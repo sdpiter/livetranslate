2045c5cc9b532bbebffd55452bdbfaccd746f956
sdpiter <sdpiter22@gmail.com>
2025-11-13 22:51:41 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-// Root build.gradle.kts
 plugins {
-    // Плагин для Android проекта
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
