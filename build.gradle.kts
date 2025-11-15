94d08183ce35329a50e2d3469b70a349f82a2ed4
Dima <sdpiter22@gmail.com>
2025-11-15 22:39:54 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
 // Root build.gradle.kts
 
 plugins {
-    // подключаем Android и Kotlin плагины с нужными версиями
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
-
-// задача очистки проекта
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    // подключаем Android и Kotlin плагины с нужными версиями
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
