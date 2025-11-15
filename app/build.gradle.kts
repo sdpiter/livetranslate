ead4678c03334a692c900b1431c368ffe9098331
Dima <sdpiter22@gmail.com>
2025-11-15 23:57:28 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,3 +80,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+// ✅ ИСПРАВЛЕНО: Добавлен блок для совместимости kapt и dataBinding
+kapt {
+    arguments {
+        arg("android.databinding.artifactType", "JAR")
+    }
+}
