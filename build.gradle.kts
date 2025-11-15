5e7feb39e639d18cb7432c0755c982f783810283
Dima <sdpiter22@gmail.com>
2025-11-15 22:47:52 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Root build.gradle.kts
 
 plugins {
-    // подключаем Android и Kotlin плагины с нужными версиями
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
